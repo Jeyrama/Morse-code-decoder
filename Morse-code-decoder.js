2af70e1944b6505dab1d631400abe30658d82a2b
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2024-12-27 09:25:28 -1000

    Update Morse-code-decoder.js

--- Morse-code-decoder.js
+++ Morse-code-decoder.js
@@ -35,3 +35,13 @@ NOTE:
 
 
 // Solution
+
+decodeMorse = function(morseCode){
+  function decodeMorseLetter(letter) {
+    return MORSE_CODE[letter];
+  }
+  function decodeMorseWord(word) {
+    return word.split(' ').map(decodeMorseLetter).join('');
+  }
+  return morseCode.trim().split('   ').map(decodeMorseWord).join(' ');
+}
